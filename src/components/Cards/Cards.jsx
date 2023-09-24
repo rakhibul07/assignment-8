e0435ff0cefb99e5ef6b9c6ec6c79b8bd5871924
rakhibul07 <rakhibulislam6996@gmail.com>
2023-09-24 23:58:00 +0600

    create card details components

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import Card from "../Card/Card";
 const Cards = ({cards}) => {
     return (
-        <div className='grid grid-cols-4 gap-4'>
+        <div className='grid grid-cols-1 px-5 lg:px-0 md:grid-cols-2 lg:grid-cols-4 gap-4'>
             {
                 cards?.map(card=><Card key={card.id} card={card}></Card>)
             }
