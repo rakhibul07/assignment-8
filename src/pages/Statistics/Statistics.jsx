62ae077518afa1eff38ae0c45e87881391d1526b
rakhibul07 <rakhibulislam6996@gmail.com>
2023-09-26 13:13:29 +0600

    update the piechart like figma

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,35 +1,63 @@
-
 import { useEffect, useState } from "react";
 import { Chart } from "react-google-charts";
+
 const Statistics = () => {
-const [donation, setDonation] = useState(0);
-useEffect(()=>{
+  const [donation, setDonation] = useState(0);
+
+  useEffect(() => {
     const donationItem = JSON.parse(localStorage.getItem("donation"));
-    if(donationItem){
-        const donationAmount = donationItem.reduce((preValue, currentItem)=>preValue + currentItem.donation_count,0);
-        setDonation(donationAmount);
+    if (donationItem) {
+      const donationAmount = donationItem.reduce(
+        (preValue, currentItem) => preValue + currentItem.donation_count,
+        0
+      );
+      setDonation(donationAmount);
     }
-   
-},[])
+  }, []);
 
-    const yourDonation = donation;
-     const data = [
-        ["Task", "Hours per Day"],
-        ["Total Donation", 12],
-        ["Your Donation", yourDonation],
-       
-      ];
-    return (
-        <div className="h-screen">
-            <Chart
-      chartType="PieChart"
-      data={data}
+  const yourDonation = donation;
 
-      width={"100%"}
-      height={"400px"}
-    />
+  const data = [
+    ["Task", "Donation"],
+    ["Total Donation", 12],
+    ["Your Donation", yourDonation],
+  ];
+
+  const lightRed = "#FF444A"; 
+  const lightBlue = "#00C49F"; 
+
+  return (
+    <div>
+      <div className=" flex flex-col">
+        <div>
+          <Chart
+            chartType="PieChart"
+            data={data}
+            options={{
+              pieSliceText: "percentage",
+              legend: "none", 
+              height: 400, 
+              colors: [lightRed, lightBlue],
+            }}
+          />
         </div>
-    );
+        <div className="flex justify-center space-x-4">
+          <div className="flex items-center">
+            <div
+              className="w-12 h-2 bg-[#00C49F] mr-2"
+            ></div>
+            <span>Your Donation</span>
+          </div>
+          <div className="flex items-center">
+            <div
+              className="w-12 h-2 bg-[#FF444A] mr-2" 
+            ></div>
+            <span>Total Donation</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
